c437c8266190290dcd5ffaa1332ccf44b3a6cb50
Alexandre Oliveira <alexandre.oliveira@viglet.com>
2021-02-19 16:08:14 -0300

    - TurMultiValue singleItem method

--- src/main/java/com/viglet/turing/wem/beans/TurMultiValue.java
+++ src/main/java/com/viglet/turing/wem/beans/TurMultiValue.java
@@ -6,4 +6,9 @@ public class TurMultiValue extends ArrayList<String> {
 
 	private static final long serialVersionUID = 1L;
 
+	public static TurMultiValue singleItem(String text) {
+		TurMultiValue turMultiValue = new TurMultiValue();
+		turMultiValue.add(text);
+		return turMultiValue;
+	}
 }
